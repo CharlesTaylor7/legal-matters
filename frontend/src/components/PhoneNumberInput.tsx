b256f4c223f84e55a7be7b9d37a83f13d88c79dd
Charles Taylor <charlestaylor95@gmail.com>
2025-05-22 11:28:45 -0400

    PHone number input

--- frontend/src/components/PhoneNumberInput.tsx
+++ frontend/src/components/PhoneNumberInput.tsx
@@ -1,30 +1,25 @@
 import React, { type InputHTMLAttributes } from "react";
-import { useIMask } from "react-imask";
+import { IMaskMixin } from "react-imask";
 
 type Props = Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> & {
-  className?: string;
   value: string;
   onChange: (value: string) => void;
 };
 
-export default function PhoneNumberInput(props: Props) {
-  const onAccept = function (...args) {
-    console.log(args);
-    props.onChange(args);
-  };
-  const mask = useIMask({
-    mask: "(000) 000-0000",
-    onAccept,
-    placeholderChar: "_",
-  });
+export default function PhoneInput(props: Props) {
+  const { onChange, ...inputProps } = props;
   return (
-    <input
-      type="tel"
-      // @ts-expect-error imask
-      ref={mask.ref}
-      className={`input input-bordered w-full ${props.className ?? ""}`}
-      value={mask.value}
-      onInput={(e) => mask.setValue(e.target.value)}
+    <MaskedStyledInput
+      mask="(000) 000-0000"
+      lazy={false}
+      unmask={true}
+      onAccept={onChange}
+      {...inputProps}
     />
   );
 }
+
+const MaskedStyledInput = IMaskMixin(({ inputRef, ...props }) => (
+  // @ts-expect-error imask's ref type doesn't match, but this is how the library is used
+  <input type="tel" className="input w-full" {...props} ref={inputRef} />
+));
